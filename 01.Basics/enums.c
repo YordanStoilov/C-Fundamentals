6dfe948f2abab6f1663b16d36f15348a9a78ed58
YordanStoilov <iordan27@gmail.com>
2024-10-30 20:24:08 +0200

    enums

---
+++ 01.Basics/enums.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// enum menu
+// {
+//     Tea = 10,
+//     Coffee = 20,
+//     Juice = 30,
+//     Beer = 40
+// };
+
+// int main()
+// {
+//     enum menu me = Coffee;
+//     enum menu them = Beer;
+
+//     printf("I ordered %d\n", me);
+//     printf("They ordered %d\n", them);
+
+//     return 0;
+// }
+
+enum months
+{
+    JAN = 1,
+    FEB,
+    MAR,
+    APR,
+    MAY,
+    JUN,
+    JUL,
+    AUG,
+    SEP,
+    OCT,
+    NOV,
+    DEC
+};
+
+int main()
+{
+    printf("My favourite month is %d\n", NOV);
+}
---
+++ ControlFlow/controlFlow.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int apples = 12;
+    int oranges = 9;
+
+    printf("A total of %d kids in class:\n", (apples + oranges));
+    printf("\t%d apples\n", apples);
+    printf("\t%d oranges\n", oranges);
+};
