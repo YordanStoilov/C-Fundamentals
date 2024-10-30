80bfe4bdf4b0cdb6f86533a90270bfc314c230e4
YordanStoilov <iordan27@gmail.com>
2024-10-30 17:22:35 +0200

    initial commit

---
+++ Examples/example.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char string[45];
+    int age = 25;
+
+    printf("Enter your name: ");
+
+    fgets(string, sizeof(string), stdin);
+
+    printf("Hello, %s\n", string);
+    
+    return 0;
+}
