96031002e372b5c31517fc656382d2c797104094
YordanStoilov <iordan27@gmail.com>
2024-10-31 12:25:32 +0200

    declaring pointers

--- 01.Basics/pointers.c
+++ 01.Basics/pointers.c
@@ -3,5 +3,22 @@
 
 int main()
 {
-    
+    int x = 60;
+
+    // Defining pointer to x:
+    int* px = &x;
+
+    // Setting y to the VALUE of x:
+    int y = *px;
+
+    // You can also define pointers along with
+    // other values, as long as the pointer points
+    // to the same type, for example:
+
+    // Declaring int p and int *q:
+    int p, *q;
+
+    // Declaring int* a and int b:
+    int* a, b;
+
 }
