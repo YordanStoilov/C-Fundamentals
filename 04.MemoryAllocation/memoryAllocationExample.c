c631f532c6a5e404bccef579dabac6d27dcc0750
YordanStoilov <iordan27@gmail.com>
2024-11-03 15:28:59 +0200

    fixed a possible memory leak error in example

--- 04.MemoryAllocation/memoryAllocationExample.c
+++ 04.MemoryAllocation/memoryAllocationExample.c
@@ -13,8 +13,22 @@ int main()
     strcpy(string, "MyName");
     printf("String = %s, Address = %p\n", string, string);
 
-    // Reallocating memory to fit a longer string
-    string = (char*)realloc(string, length + 4);
+    // Reallocating memory to fit a longer string:
+
+    // Always use a temporary variable, because in case of
+    // not enough memory, realloc returns null
+    // and this will lead to a memory leak
+    char* temp = (char*)realloc(string, length + 4);
+    
+    if (temp)
+        // If temp is not NULL, assign to it the value of temp
+        string = temp;
+    else
+    {
+        // If it's null, print error message
+        fprintf(stderr, "Memory reallocation failed\n");
+        return -1;
+    }
 
     // Concatenate the second string to the first one
     strcat(string, ".com");
